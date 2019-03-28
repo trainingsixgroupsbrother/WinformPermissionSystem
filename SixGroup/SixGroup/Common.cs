c15b3e72f6484ae4c0a875128cf0b891f605c525
李立飞 <1933559183@qq.com>
2019-03-28 22:51:46 +0800

    添加一个通用类文件

---
+++ SixGroup/SixGroup/Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixGroup
+{
+    public class Common
+    {
+        //就单纯的测试一下,提交文件
+    }
+}
